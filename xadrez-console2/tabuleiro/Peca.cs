ba8c6eda5fc725de5a746999bd43b8be9fe27273
Raphael Fernandes <holyfernandes@yahoo.co.uk>
2020-07-31 18:57:17 -0300

    Classes de posição, cor, peça e tabuleiro criadas.

---
+++ xadrez-console2/tabuleiro/Peca.cs
@@ -0,0 +1,19 @@
+﻿
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; } //somente pode ser acessada pela subclasse dela
+        public int _qteMovimentos { get; protected set; }
+        public Tabuleiro Tabuleiro { get; protected set; }
+
+        public Peca(Posicao posicao, Tabuleiro tabuleiro, Cor cor)
+        {
+            Posicao = posicao;
+            Cor = cor;            
+            Tabuleiro = tabuleiro;
+            _qteMovimentos = 0;
+        }
+    }
+}
---
+++ xadrez-console2/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,20 @@
+﻿
+namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+        private Peca[,] Peca { get; set; }
+
+        public Tabuleiro(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+            //A variável Peca é criado como matriz e recebe de parâmetro as linhas e colunas
+            //do construtor.
+            Peca = new Peca[linha, coluna]; 
+        }
+
+    }
+}
