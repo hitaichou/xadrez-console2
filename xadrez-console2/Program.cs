fa03346aadb8e03b2d2c114b68cddbd1ba41c4a1
Raphael Fernandes <holyfernandes@yahoo.co.uk>
2020-08-01 10:08:35 -0300

    Melhorado método de imprimir tabuleiro. Adicionado cores as peças.

--- xadrez-console2/Program.cs
+++ xadrez-console2/Program.cs
@@ -8,9 +8,9 @@ namespace xadrez_console2
     {
         static void Main(string[] args)
         {
-            /*try
+            try
             {
-                Tabuleiro tab = new Tabuleiro(8, 8);*/
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
             /*Criado subclasse da Torre e Rei
              * Dentro da namespace xadrez
@@ -19,23 +19,23 @@ namespace xadrez_console2
              * Quem atribui a posição a peça é o método Tabuleiro.colocarPeca
              * E o método é chamado aqui no programa principal
              */
-            /*tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 1));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(6, 6));
 
-            Tela.imprimirTabuleiro(tab);*/
+
+                Tela.imprimirTabuleiro(tab);
 
 
-            /*}
+            }
             catch(Exception e)
             {
                 Console.WriteLine(e.Message);
-            }*/
+            }
 
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-            Console.WriteLine(pos);
-
-            Console.WriteLine(pos.toPosicao());
+            
 
         }
     }
--- xadrez-console2/Tela.cs
+++ xadrez-console2/Tela.cs
@@ -10,21 +10,45 @@ namespace xadrez_console2
             //crio matriz para criar o tabuleiro na tela
             for(int i = 0; i < tab.linhas; i++)
             {
+                Console.Write(8 - i + " "); //insiro o número da linha do tabuleiro
+
                 for(int j = 0; j < tab.colunas; j++)
                 {
+                    //Console.Write('a' - j + " ");//insiro a letra nas colunas do tabuleiro
+
                     if (tab.peca(i,j) == null) //valido se a posição está nula
                     {
                         Console.Write("- ");                        
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        //chamo o método para imprimir peça na cor desejada
+                        imprimiPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
                     
                 }
                 Console.WriteLine();
             }
-           
+            Console.WriteLine("  a b c d e f g h");           
+        }
+
+        //Método para mudar a cor da peça
+        public static void imprimiPeca(Peca peca)
+        {
+            //se a peça for branca, mantém a sua cor
+            if(peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor; //salvo a cor padrão de fundo na variável
+                Console.ForegroundColor = ConsoleColor.Yellow; //mudo o fundo para uma outra cor
+                Console.Write(peca); //imprimo a peça
+                Console.ForegroundColor = aux; //volto a cor de fundo original
+            }
+
         }
 
     }
